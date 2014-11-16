556e19424dfab9ff15ea030200c8abc20d949ffe
Nicolas Besnard <besnard.nicolas@gmail.com>
2014-11-16 17:08:56 +0000

    Don't repeat request on each visit

--- background.js
+++ background.js
@@ -1,3 +1,39 @@
+console.log("Load extension");
+
+var Database = {
+	addEpisode: function(episode, leechers, seeders, magnet)
+	{
+		console.log("addEpisode", episode);
+		localStorage[episode +'-leechers'] = leechers;
+		localStorage[episode +'-seeders'] = seeders;
+		localStorage[episode +'-magnet'] = magnet;
+		localStorage[episode +'-date'] = Math.floor((Date.now() / 1000));
+	},
+
+	episodeIsDefine: function(episode)
+	{
+		// Magnet exist
+		if (episode +'-magnet' in localStorage)
+		{
+			// Magnet is not more than 1 days old (86400 secs)
+			if (Math.floor(Date.now() / 1000) - localStorage[episode +'-date'] < 86400)
+			{
+				return true;
+			}
+		}
+		return false;
+	},
+
+	getEpisode: function(episode)
+	{
+		return {
+			"magnet": localStorage[episode +'-magnet'],
+			"seeders": localStorage[episode +'-seeders'],
+			"leechers": localStorage[episode +'-leechers'],
+		};
+	}
+};
+
 function Episode(episode, serieName, thisEpisode)
 {
 	this.episode = episode;
@@ -7,7 +43,6 @@ function Episode(episode, serieName, thisEpisode)
 
 Episode.prototype.getMagnet = function()
 {
-	console.log("getMagnet", this.isViewed());
 	if (this.isViewed() == false)
 	{
 		this.fetchMaget();
@@ -21,16 +56,33 @@ Episode.prototype.isViewed = function()
 
 Episode.prototype.fetchMaget = function()
 {
-	var that = this;
-	console.log(this.episode);
-	$.get('http://pirateproxy.ws/search/'+ this.serieName +' '+ this.thisEpisode +'/0/7/208', function(data)
+	if (Database.episodeIsDefine(this.serieName + this.thisEpisode))
 	{
-		var doc = $(data);
-		var magnet = doc.find('a[title="Download this torrent using magnet"]').first();
-		var seeders = $(magnet.parents('tr').find('td[align=right]')[0]).html();
-		var leechers = $(magnet.parents('tr').find('td[align=right]')[1]).html();
-		that.episode.find('.srtlinks').append('<a href="' + magnet.attr('href') + '"> Télécharger le torrent ↓</a> (<span style="color: #008f0d;">' + seeders + '</span> / <span style="color: #b94309;">' + leechers + '</span>)');
-	});
+		var episode = Database.getEpisode(this.serieName + this.thisEpisode);
+
+		this.setHTML(episode['magnet'], episode['seeders'], episode['leechers']);
+	}
+	else
+	{
+		var that = this;
+
+		$.get('http://pirateproxy.ws/search/'+ this.serieName +' '+ this.thisEpisode +'/0/7/208', function(data)
+		{
+			var doc = $(data);
+			var magnet = doc.find('a[title="Download this torrent using magnet"]').first();
+			var seeders = $(magnet.parents('tr').find('td[align=right]')[0]).html();
+			var leechers = $(magnet.parents('tr').find('td[align=right]')[1]).html();
+
+			Database.addEpisode(that.serieName + that.thisEpisode, seeders, leechers, magnet);
+
+			that.setHTML(magnet.attr('href'), seeders, leechers);
+		});
+	}
+};
+
+Episode.prototype.setHTML = function(magnet, seeders, leechers)
+{
+	this.episode.find('.srtlinks').append('<a href="' + magnet + '"> Télécharger le torrent ↓</a> (<span style="color: #008f0d;">' + seeders + '</span> / <span style="color: #b94309;">' + leechers + '</span>)');
 };
 
 var handleEpisodes = function($listOfEpisodes)
@@ -39,7 +91,7 @@ var handleEpisodes = function($listOfEpisodes)
 	{
 		var episode = $($listOfEpisodes[i]);
 		var serieName = episode.find('.titre .ep').html();
-		var thisEpisode = episode.find('.titre .ep').html();
+		var thisEpisode = episode.find('.titre .ep').next().html();
 
 		var my = new Episode(
 			episode,
@@ -48,10 +100,13 @@ var handleEpisodes = function($listOfEpisodes)
 			i
 		);
 		my.getMagnet();
+		if (i == 3)
+			break;
 	}
 }
 
 var episodes_search_is_launched = false
+
 var observer = new MutationObserver(function(mutations)
 {
 	mutations.forEach(function(mutation) {
@@ -61,7 +116,7 @@ var observer = new MutationObserver(function(mutations)
 			var episodesThisWeek = $($('#planning_container .episodes h2')[1]).nextUntil('.clear')
 
 			handleEpisodes(episodesLastWeek);
-			handleEpisodes(episodesThisWeek);
+			// handleEpisodes(episodesThisWeek);
 
 			episodes_search_is_launched = true;
 		}
@@ -69,8 +124,8 @@ var observer = new MutationObserver(function(mutations)
 });
 
 observer.observe(document.getElementById('planning_container'), {
-    childList: true
-  , subtree: true
-  , attributes: false
-  , characterData: false
+    childList: true,
+  	subtree: true,
+  	attributes: false,
+  	characterData: false
 })
